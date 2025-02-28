ab69bffc623b24814edeababefd5e93e08b5fed1
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-02-28 20:58:47 +0530

    backend token fixed

--- Backend/BookNexus_API/BookNexus_API/settings.py
+++ Backend/BookNexus_API/BookNexus_API/settings.py
@@ -52,6 +52,8 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
+    # 'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=12),  
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),  
     
